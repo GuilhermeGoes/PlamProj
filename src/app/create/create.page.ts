e599aae9cc312c9367429190fd25819ff1acfcbe
Fulano de Tal <fulanodetal@exemplo.br>
2020-11-23 22:57:51 -0300

    feita a função de salvar anotações

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { note } from '../anotacoes/anotacoes.page';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
+  //criação da função de salvar as anotações
+  public emptyNote: note = {
+    title: '',
+    description: ''
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleSave(){
+    console.log(this.emptyNote);
   }
 
 }
