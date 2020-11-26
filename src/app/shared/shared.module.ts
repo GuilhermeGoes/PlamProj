0c264c6c146ffac4cbb9c3ee0b5d7ea5e7ff6b9a
GuilhermeGoes <guilhermegoes76@hotmail.com>
2020-11-26 00:23:19 -0300

    adicionando componente header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,19 @@ import { CommonModule } from '@angular/common';
 import { TabBarComponent } from './components/tab-bar';
 import { RouterModule } from '@angular/router';
 import { NotesFormComponent } from './components/notes-form/notes-form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [NotesFormComponent, TabBarComponent],
-  exports: [NotesFormComponent, TabBarComponent],
+  declarations: [
+    NotesFormComponent, 
+    TabBarComponent, 
+    HeaderComponent
+  ],
+  exports: [
+    NotesFormComponent, 
+    TabBarComponent, 
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
