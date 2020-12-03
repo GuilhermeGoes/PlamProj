843a4ce033ff389443fe6f90b6e558a62d786808
GuilhermeGoes <guilhermegoes76@hotmail.com>
2020-12-03 04:29:06 -0300

    persistindo dado em collection

--- src/app/services/collection.service.ts
+++ src/app/services/collection.service.ts
@@ -20,15 +20,11 @@ export interface Collection {
 })
 export class CollectionService {
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage) { 
+    this.fetchFromStorage();
+  }
 
-  private collections: Collection[] = [
-    // { id: 1, title: 'Diretivas - 17.09 - Revisão', image: '../../assets/diretivas.jpg'},
-    // { id: 2, title: 'Injeção de dependências  - 24.09 - Revisão', image: '../../assets/injecaodependencias.jpg'},
-    // { id: 3, title: 'Past Continuous - Revisão', image: '../../assets/pastContinuous.png'}
-  ];
-
-   //private collections: Collection[] = [];
+  private collections: Collection[] = [];
 
   public addCollection(collection: Collection){
     const maxId = Math.max(...this.collections.map(n => n.id), 0);
@@ -40,20 +36,28 @@ export class CollectionService {
       image: collection.image,
       cards: collection.cards
     });
-    
+
     this.storeData();
+  }
+
+  private async fetchFromStorage(){
+    this.collections.push(...(await this.storage.get('collections')));
+  
   }
 
   private storeData() {
     this.storage.set('collections', this.collections);
+  
   }
 
   public getCollections() {
     return this.collections;
+  
   }
 
   public find(id : number){
     return {...this.collections.find(c => c.id === id)};
+  
   }
 
 }
