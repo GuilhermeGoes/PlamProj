452ebdc3d28254e1b87cc1835156a30a1d4f59b3
GuilhermeGoes <guilhermegoes76@hotmail.com>
2020-12-03 02:28:47 -0300

    criando classe modelo de cartão

---
+++ src/app/shared/models/card.ts
@@ -0,0 +1,9 @@
+export class Card {
+    
+    constructor(public id: number = 0,
+                public front: string = '',
+                public back: string = '',
+                public quantity: number = 0) {
+
+    }
+}
